8b651de94d97e424cc57a4667720b791cec99e10
Chrbknudsen <christian.knudsen@gmail.com>
2023-04-10 11:51:21 +0200

    links tilføjet

--- script.R
+++ script.R
@@ -27,11 +27,30 @@ resultater <- POST( endpoint, body = body, encode = "json")
 data <- resultater %>% 
   content(type  ="text") %>% read_csv2()
 
+data  %>% 
+  pivot_wider(names_from = KØN, values_from = INDHOLD) %>% 
+  mutate(dif = Kvinder - Mænd)  %>% 
+  pivot_longer(2:4, names_to = "type", values_to = "value") %>%  
+  ggplot(aes(TID, value, color = type, group = type))+
+  geom_line()
 
-view(data)  
+
 data %>% 
-  ggplot(aes(x = TID, y = INDHOLD)) +
-  geom_point()
+  ggplot(aes(x = TID, y = INDHOLD, color = KØN)) +
+  geom_point() 
 
 
 https://www.esundhed.dk/Emner/Hvad-doer-vi-af/Doedsaarsager
+
+
+https://www.listendata.com/2019/06/create-infographics-with-r.html
+https://www.r-bloggers.com/2015/02/r-how-to-layout-and-design-an-infographic/
+  https://www.trm.dk/nyheder/2022/ny-undersoegelse-om-omfanget-af-vold-og-overgreb-i-danmark-2021
+
+
+if(capabilities()[["X11"]]) withAutoprint({
+  X11Fonts()
+  X11Fonts("mono")
+  utopia <- X11Font("-*-utopia-*-*-*-*-*-*-*-*-*-*-*-*")
+  X11Fonts(utopia = utopia)
+})
